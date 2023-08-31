700d361acec2e1b06e7d6e26b60e590b2af68247
Sebastian Vasquez <sebitas0489@gmail.com>
2023-08-30 22:28:27 -0500

    Método sort y finalización del curso.

---
+++ server/18-sort.js
@@ -0,0 +1,46 @@
+//** El método sort es mutable y consiste en ordenar un array a partir de los valores Unicode de los caracteres y este retorna un array con la misma referencia en memoria que el original. */
+
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort() 
+console.log(months);
+//////////////////////
+const numbers = [1, 30, 4, 21, 100000];
+numbers.sort((a,b) => a - b);
+console.log(numbers);
+//////////////////////
+const words = [
+    "réservé",
+    "premier",
+    "communiqué",
+    "café",
+    "adieu",
+    "éclair",
+    "banana",
+];
+words.sort();
+console.log(words);
+//////////////////////
+const orders = [
+    {
+        customerName: "Nicolas",
+        total: 600,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 1840,
+        delivered: true,
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+    },
+];
+orders.sort((a,b) => b.total - a.total);
+console.log(orders);
